0640830ce82f3192a10bb08f6a3cb4255ec35c4a
mikopits <mikopits@gmail.com>
2017-05-16 09:25:06 -0700

    Add opencl development files to its own branch.

---
+++ src/opencl/mod.rs
@@ -0,0 +1,3 @@
+mod error;
+mod ops;
+mod openclhandler;
---
+++ src/opencl/ops.rs
@@ -0,0 +1,68 @@
+use std::cmp;
+use ::{FromPrimitive, Num, ToPrimitive};
+use ::{DenseMatrix, IdentityMatrix, Matrix, SparseMatrix, ZeroMatrix};
+
+use ocl::{flags, Platform, Device, Context}
+
+use::{OpenCLHandler}
+
+ ////////////////////////////////////////////////////////////////////////////////
+// Container trait for Matrix operation Kernel Strings
+// Supported:   add
+//              equal
+//              subtract
+//              multiply
+////////////////////////////////////////////////////////////////////////////////
+
+pub trait Ops {
+
+    // configure statics for opencl kkernel program eg. #define ...
+    #[inline]
+    fn generate_kernel_config(&self, source : &String);
+
+    // Opn Dense Matrices
+    #[inline]
+    fn add_kernel(&self, source: &String, numeric_type: &String){ println!("{}") }
+    #[inline]
+    fn eq_kernel(&self, source: &String, numeric_type: &String){ println!("{}") }
+    #[inline]
+    fn mul_kernel(&self, source: &String, numeric_type: &String){ println!("{}") }
+
+}
+
+// TODO: add kernels here
+// impl Ops for DenseMatrix<T>
+// {
+//     fn eq_kernel(){
+//
+//     }
+//
+//
+//     fn generate_kernel_config(&self, source : &String){
+//
+//     }
+// }
+//
+// impl Ops for SparseMatrix<T>
+// {
+//     fn eq_kernel(){
+//
+//     }
+//
+//
+//     fn generate_kernel_config(&self, source : &String){
+//
+//     }
+// }
+//
+// impl Ops for ZeroMatrix<T>
+// {
+//     fn eq_kernel(){
+//
+//     }
+//
+//
+//     fn generate_kernel_config(&self, source : &String){
+//
+//     }
+// }
